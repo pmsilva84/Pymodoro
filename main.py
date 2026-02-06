d5600a7f3117fbb001ddd46c82223d05ef5a6da2
Pedroca <pedroms5+github@protonmail.com>
2026-02-06 11:05:36 -0300

    Funções de Foco, Descanço curto e longo foram criadas

--- main.py
+++ main.py
@@ -1,52 +1,73 @@
 import os
 import time
 from pygame import mixer
+import pyfiglet
+
+## Essa sequencia de funçoes é só para fazer o codigo ficar mais simple(para mim)
+def time2focus():
+    focusTime = 25 * 60
+    
+    for i in range(focusTime,-1,-1):
+        os.system('clear')
+        min,sec = divmod(i,60)
+        print(pyfiglet.figlet_format(f"{min:02} : {sec:02}", font= "computer"))
+        time.sleep(1)
+
+    mixer.music.play()
+
+def time2shortrest():
+    shortRest = 5 * 60
+    for i in range(shortRest,-1,-1):
+        os.system('clear')
+        min,sec = divmod(i,60)
+        print(pyfiglet.figlet_format(f"{min:02} : {sec:02}", font= "computer"))
+        time.sleep(1)
+
+    mixer.music.play()
+
+def time2longrest():
+    longRest = 15 * 60
+
+    for i in range(longRest,-1,-1):
+        os.system('clear')
+        min,sec = divmod(i,60)
+        print(pyfiglet.figlet_format(f"{min:02} : {sec:02}", font= "computer"))
+        time.sleep(1)
+
+    mixer.music.play()
+
 
 mixer.init()
 mixer.music.load("Pymodoro/erro.mp3")
 
-focusTime = 25 * 60
-shortRest = 5 * 60
-longRest = 15 * 60
+time2focus()
 
-for i in range(focusTime,-1,-1):
+os.system("clear")
+continueOption = input("Continuar?\n")
+if continueOption == "S": time2shortrest() 
+else:
     os.system('clear')
-    min,sec = divmod(i,60)
-    print(f"{min:02}:{sec:02}")
-    time.sleep(1)
-mixer.music.play()
 
-for i in range(shortRest,-1,-1):
+os.system("clear")
+continueOption = input("Continuar?\n")
+if continueOption == "S": time2focus() 
+else:
     os.system('clear')
-    min,sec = divmod(i,60)
-    print(f"{min:02}:{sec:02}")
-    time.sleep(1)
-mixer.music.play()
 
-for i in range(focusTime,-1,-1):
+os.system("clear")
+continueOption = input("Continuar?\n")
+if continueOption == "S": time2shortrest() 
+else:
     os.system('clear')
-    min,sec = divmod(i,60)
-    print(f"{min:02}:{sec:02}")
-    time.sleep(1)
-mixer.music.play()
 
-for i in range(shortRest,-1,-1):
+os.system("clear")
+continueOption = input("Continuar?\n")
+if continueOption == "S": time2focus() 
+else:
     os.system('clear')
-    min,sec = divmod(i,60)
-    print(f"{min:02}:{sec:02}")
-    time.sleep(1)
-mixer.music.play()
 
-for i in range(focusTime,-1,-1):
+os.system("clear")
+continueOption = input("Continuar?\n")
+if continueOption == "S": time2longrest() 
+else:
     os.system('clear')
-    min,sec = divmod(i,60)
-    print(f"{min:02}:{sec:02}")
-    time.sleep(1)
-mixer.music.play()
-
-for i in range(longRest,-1,-1):
-    os.system('clear')
-    min,sec = divmod(i,60)
-    print(f"{min:02}:{sec:02}")
-    time.sleep(1)
-mixer.music.play()
