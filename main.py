2045f3387e8696602bd36392c27523b9549b926b
pmsilva84 <pedroms5+github@protonmail.com>
2026-02-21 19:12:25 -0300

    Some silly changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import function as pom
 import os
 import pyfiglet
 
+os.system("clear")
 print(pyfiglet.figlet_format("Pymodoro", font= "computer")) ## A bendiata do menu iniciar 
 print("Made by P.A." + ("\n"*2))
 
@@ -11,7 +12,8 @@ try :
     if start == "Y" or "y": 
         pom.chronoTimer()
         
-    else: 
+    elif start == "N" or "n": 
+        os.system("clear")
         print("bye bye...")
 
 except KeyboardInterrupt:
